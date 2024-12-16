348e4fbef11ed36dd1fd116a62edc7acfd3667cd
Guilherme Campos <guiicampos31@gmail.com>
2024-12-16 18:05:33 -0300

    UserRepository DataBase seeding

---
+++ src/main/java/com/pessoal/estudos/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pessoal.estudos.repositories;
+
+import com.pessoal.estudos.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
